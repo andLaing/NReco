f09aaea901883f4221b2409deaaa59850dae9adb
andLaing <a.b.r.laing@gmail.com>
2022-03-30 12:08:13 +0200

    Add checks of number of MC true vertices

--- scripts/sipm_rate.jl
+++ scripts/sipm_rate.jl
@@ -15,6 +15,7 @@ using HDF5
 import Base.@kwdef
 @kwdef struct SiPMEvt <: ATools.OutputDataset
     nevt ::Int64   = one(Int64)
+    nvtx ::Int64   = zero(Int64)
     nsipm::Int64   = zero(Int64)
     tmin ::Float32 = zero(Float32)
     tmax ::Float32 = zero(Float32)
@@ -35,6 +36,20 @@ function lor_kmeansTest(hitdf::DataFrame, k::Int64)
 	mean(silhouettes(kr, distances))
 end
 
+function count_vertices(vertices::GroupedDataFrame, vols::Vector{Int64}, evts::Vector{Int64})
+    vtx_count = 0
+    for evt in evts
+        try
+            vrtx = vertices[(evt,)] 
+        catch
+            continue
+        end
+        vrtx = filter(df -> in(vols).(df.volume_id), vrtx)
+        vtx_count += length(unique(vrtx.track_id))
+    end
+    vtx_count
+end
+
 function sipm_rate(file_path::String, outfile::String, dconf::NReco.DetConf,
                    source_rate::Float32, evt_window::Float32)
 
@@ -46,9 +61,12 @@ function sipm_rate(file_path::String, outfile::String, dconf::NReco.DetConf,
 
         simd_events    += nrow(pdf.primaries)
         mixed_waveforms = time_bins(pdf.primaries[:, [:event_id]], pdf.waveform)
-        # for wvf in groupby(pdf.waveform, :event_id)
+        grp_vrtx = groupby(filter(df -> df.parent_id .== 0, pdf.vertices), :event_id)
+        volumes  = findall((pdf.volume_names .==     "LXe") .|
+						   (pdf.volume_names .== "Steel_1")   ) .- 1
         for wvf in groupby(mixed_waveforms, :time_bin)
             kvals = Float32[0.0, 0.0, 0.0]
+            nvtx  = 0
             reco_hits  = NReco.select_sensors(wvf, dconf.ecut, dconf.pde, dconf.sigma_tof)
             if !isnothing(reco_hits) && !isempty(reco_hits)
                 nevt     = length(unique(wvf.event_id))
@@ -56,8 +74,10 @@ function sipm_rate(file_path::String, outfile::String, dconf::NReco.DetConf,
                 min_maxt = combine(reco_hits, :mtime => (x -> [extrema(x)]) => [:minv, :maxv])
                 tmin     = minimum(min_maxt.minv) 
                 tmax     = maximum(min_maxt.maxv)
-                xyzqt = NReco.sensor_positions(reco_hits, pdf.sensor_xyz)
+                xyzqt    = NReco.sensor_positions(reco_hits, pdf.sensor_xyz)
                 if nrow(xyzqt) > 4
+                    evts = unique(wvf.event_id)
+                    nvtx = count_vertices(grp_vrtx, volumes, evts)
                     #_, _, dotdf1, dotdf2 = NReco.lor_maxq(xyzqt)
                     #println("nevent $nevt, dot product split df1 sensors = $(nrow(dotdf1)), df2 sensors = $(nrow(dotdf2))")
                     for k in 2:4
@@ -68,7 +88,7 @@ function sipm_rate(file_path::String, outfile::String, dconf::NReco.DetConf,
                 push!(evt_sipm, SiPMEvt(nevt  = nevt    , nsipm = nsens   ,
                                         tmin  = tmin    , tmax  = tmax    ,
                                         k2sil = kvals[1], k3sil = kvals[2],
-                                        k4sil = kvals[3]))
+                                        k4sil = kvals[3], nvtx  = nvtx    ))
             end
         end
     end
